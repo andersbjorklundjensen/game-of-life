9a0b8fcb31170b0984f7607c9006c1769727d2a8
root <andersbjorklund@live.com>
2020-12-26 12:04:54 +0000

    Initialize cell class

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,13 @@
 import Game from './Game';
+import Cell from './Cell';
 
 describe('Game class tests', () => {
-  it('should initialize a game with a grid', () => {
+  it('should initialize a game with a grid of cell objects', () => {
+    const grid = [
+      [new Cell(), new Cell()],
+      [new Cell(), new Cell()],
+    ];
+
     const game = new Game();
   })
 })
