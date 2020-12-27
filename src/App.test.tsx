4ea7bb7c0b74d4e721a89530618d59959c3cebc2
root <andersbjorklund@live.com>
2020-12-27 16:28:12 +0000

    Add an variable grid counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component tests', () => {
@@ -8,5 +8,25 @@ describe('App component tests', () => {
     const headline = screen.getByRole('heading');
 
     expect(headline).toHaveTextContent('Game of life');
+  });
+
+  it('renders an adjustable grid size variable', () => {
+    render(<App />);
+
+    const incrementButton = screen.getByRole('button', {
+      name: '+'
+    });
+    const decrementButton = screen.getByRole('button', {
+      name: '-'
+    });
+    const counter = screen.getByTestId('gridSize')
+
+    expect(counter).toHaveTextContent('0');
+
+    fireEvent.click(incrementButton);
+    expect(counter).toHaveTextContent('1');
+
+    fireEvent.click(decrementButton);
+    expect(counter).toHaveTextContent('0');
   });
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [gridSize, setGridSize] = useState(0)
   return (
     <div>
       <h1>Game of life</h1>
+      <div data-testid="gridSize">{gridSize}</div>
+      <button onClick={() => setGridSize(prev => prev + 1)}>+</button>
+      <button onClick={() => setGridSize(prev => prev - 1)}>-</button>
     </div>
   )
 }
